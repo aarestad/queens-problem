f8b839367451337c8cd6f03736828114429bd2b1
Peter Aarestad <aarestad@gmail.com>
2013-08-10 13:32:59 -0500

    whitespace etc.

--- board.rb
+++ board.rb
@@ -1,84 +1,84 @@
 # TODO make it easier to switch between the two queens problems (no queens attacking each other vs queens attacking all squares)
 
 class Board
-	def initialize(board_size, num_queens)
-		@num_queens = num_queens
-		@board_size = board_size
-		
-		@queens = []
-		@attacked_squares = Array.new(@board_size) {Array.new(@board_size, false)}
-	end
-	
-	def queens
-		Array.new(@queens).freeze
-	end
-	
-	def add_queen(queen_x, queen_y)
-		new_queen = [queen_x, queen_y]
-	
-		if @queens.include?(new_queen)
-			@queens.delete(new_queen)
-		elsif @queens.length < @num_queens
-			@queens.push(new_queen)
-		end
-		
-		update_attacked
-	end
-	
-	def update_attacked
-		(0..@board_size-1).each do |row|
-			(0..@board_size-1).each do |col|
-				@attacked_squares[row][col] = false
-			end
-		end
-	
-		@queens.each do |queen_x, queen_y|
-			row_offset = 1
-			
-			while queen_y - row_offset >= 0
-				if queen_x - row_offset >= 0
-					@attacked_squares[queen_x - row_offset][queen_y - row_offset] = true
-				end
-				
-				if queen_x + row_offset < @board_size
-					@attacked_squares[queen_x + row_offset][queen_y - row_offset] = true
-				end
-				
-				row_offset += 1
-			end
-			
-			row_offset = 1
-			
-			while queen_y + row_offset < @board_size
-				if queen_x - row_offset >= 0
-					@attacked_squares[queen_x - row_offset][queen_y + row_offset] = true
-				end
-				
-				if queen_x + row_offset < @board_size
-					@attacked_squares[queen_x + row_offset][queen_y + row_offset] = true
-				end
-				
-				row_offset += 1	
-			end
-		
-			(0..@board_size-1).each do |row_or_col|
-				@attacked_squares[queen_x][row_or_col] = true
-				@attacked_squares[row_or_col][queen_y] = true
-			end
-		end
-	end
-	
-	def all_squares_attacked?
-		@attacked_squares.flatten.all?
-	end
-	
-	def square_attacked?(row, col)
-		@attacked_squares[row][col]
-	end
-	
-	def queen_here?(row, col)
-		@queens.include?([row, col])
-	end
-	
-	private :update_attacked
+  def initialize(board_size, num_queens)
+    @num_queens = num_queens
+    @board_size = board_size
+
+    @queens = []
+    @attacked_squares = Array.new(@board_size) { Array.new(@board_size, false) }
+  end
+
+  def queens
+    Array.new(@queens).freeze
+  end
+
+  def add_queen(queen_x, queen_y)
+    new_queen = [queen_x, queen_y]
+
+    if @queens.include?(new_queen)
+      @queens.delete(new_queen)
+    elsif @queens.length < @num_queens
+      @queens.push(new_queen)
+    end
+
+    update_attacked
+  end
+
+  def update_attacked
+    (0..@board_size-1).each do |row|
+      (0..@board_size-1).each do |col|
+        @attacked_squares[row][col] = false
+      end
+    end
+
+    @queens.each do |queen_x, queen_y|
+      row_offset = 1
+
+      while queen_y - row_offset >= 0
+        if queen_x - row_offset >= 0
+          @attacked_squares[queen_x - row_offset][queen_y - row_offset] = true
+        end
+
+        if queen_x + row_offset < @board_size
+          @attacked_squares[queen_x + row_offset][queen_y - row_offset] = true
+        end
+
+        row_offset += 1
+      end
+
+      row_offset = 1
+
+      while queen_y + row_offset < @board_size
+        if queen_x - row_offset >= 0
+          @attacked_squares[queen_x - row_offset][queen_y + row_offset] = true
+        end
+
+        if queen_x + row_offset < @board_size
+          @attacked_squares[queen_x + row_offset][queen_y + row_offset] = true
+        end
+
+        row_offset += 1
+      end
+
+      (0..@board_size-1).each do |row_or_col|
+        @attacked_squares[queen_x][row_or_col] = true
+        @attacked_squares[row_or_col][queen_y] = true
+      end
+    end
+  end
+
+  def all_squares_attacked?
+    @attacked_squares.flatten.all?
+  end
+
+  def square_attacked?(row, col)
+    @attacked_squares[row][col]
+  end
+
+  def queen_here?(row, col)
+    @queens.include?([row, col])
+  end
+
+  private :update_attacked
 end
--- test/board_spec.rb
+++ test/board_spec.rb
@@ -1,64 +1,64 @@
 require '../board'
 
 describe Board do
-    describe "a new board" do
-        it "should have no queens and no attacked squares" do
-            board = Board.new 8, 8
-            board.queens.length.should == 0
-            
-            (0..7).each do |row|
-                (0..7).each do |col|
-                    board.square_attacked?(row, col).should be false
-                end
-            end
+  describe "a new board" do
+    it "should have no queens and no attacked squares" do
+      board = Board.new 8, 8
+      board.queens.length.should == 0
+
+      (0..7).each do |row|
+        (0..7).each do |col|
+          board.square_attacked?(row, col).should be false
         end
+      end
     end
-    
-    describe "adding queens" do
-        it "should add a queen at the correct place" do
-            board = Board.new 3, 1
-            board.queens.length.should == 0
-            
-            board.add_queen(1, 0)
-            board.queens.length.should == 1
-            board.queens[0].should == [1, 0]
-            board.queen_here?(1, 0).should be true
-        end
-    
-        it "should not let you add more queens than specified" do
-            board = Board.new 3, 0
-            board.queens.length.should == 0
-            
-            board.add_queen(0, 0)
-            board.queens.length.should == 0
-        end
-        
-        it "should update the attacked squares" do
-            board = Board.new 3, 1
-            
-            (0..2).each do |row|
-                (0..2).each do |col|
-                    board.square_attacked?(row, col).should be false
-                end
-            end
-            
-            board.add_queen(1, 1)
-            
-            (0..2).each do |row|
-                (0..2).each do |col|
-                    board.square_attacked?(row, col).should be true
-                end
-            end
-        end
+  end
+
+  describe "adding queens" do
+    it "should add a queen at the correct place" do
+      board = Board.new 3, 1
+      board.queens.length.should == 0
+
+      board.add_queen(1, 0)
+      board.queens.length.should == 1
+      board.queens[0].should == [1, 0]
+      board.queen_here?(1, 0).should be true
     end
-    
-    describe "#all_squares_attacked?" do
-        it "should report true or false correctly" do
-            board = Board.new 3, 1
-            board.all_squares_attacked?.should be false
-            
-            board.add_queen(1, 1)
-            board.all_squares_attacked?.should be true
-        end
+
+    it "should not let you add more queens than specified" do
+      board = Board.new 3, 0
+      board.queens.length.should == 0
+
+      board.add_queen(0, 0)
+      board.queens.length.should == 0
     end
+
+    it "should update the attacked squares" do
+      board = Board.new 3, 1
+
+      (0..2).each do |row|
+        (0..2).each do |col|
+          board.square_attacked?(row, col).should be false
+        end
+      end
+
+      board.add_queen(1, 1)
+
+      (0..2).each do |row|
+        (0..2).each do |col|
+          board.square_attacked?(row, col).should be true
+        end
+      end
+    end
+  end
+
+  describe "#all_squares_attacked?" do
+    it "should report true or false correctly" do
+      board = Board.new 3, 1
+      board.all_squares_attacked?.should be false
+
+      board.add_queen(1, 1)
+      board.all_squares_attacked?.should be true
+    end
+  end
 end
